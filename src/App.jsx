5f50f52da1e820ae0a5e7217225d3cc37331ba4d
rhodinemma <rhodinemma10@gmail.com>
2023-03-01 22:37:29 +0300

    feat: create popular categories section

--- src/App.jsx
+++ src/App.jsx
@@ -110,6 +110,293 @@ function App() {
           </div>
         </div>
       </div>
+
+      <div>
+        <div className="bg -gray-50 py-32">
+          <div className="w-9/12 mx-auto">
+            <div className="text-center mb-12">
+              <p className="text-4xl font-bold text-gray-600 mb-3">
+                Popular Places
+              </p>
+              <p className="text-gray-600">Discover Most Searched Places</p>
+            </div>
+
+            <div className="grid grid-cols-3 gap-6 mx-auto">
+              <div className="bg-white rounded-md shadow mx-w-xl overflow-hidden">
+                <div className="h-48 overflow-hidden">
+                  <img
+                    src="/public/images/skynews-dentist-generic_5393494.jpg"
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="px-6 py-4 space-y-2">
+                  <div className="flex items-center text-sm">
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">
+                      star_half
+                    </span>
+                    <span className="ml-2 text-xs text-gray-600">
+                      294 Reviews
+                    </span>
+                  </div>
+                  <p className="text-lg font-bold text-gray-600">
+                    Dr. Someone Clinic
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    <span className="material-icons align-middle text-gray-500 -ml-1">
+                      place
+                    </span>
+                    {""} Somewhere, Uganda
+                  </p>
+                  <div className="flex gap-3">
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      clinic
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      hospital
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      $$
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-white rounded-md shadow mx-w-xl overflow-hidden">
+                <div className="h-48 overflow-hidden">
+                  <img
+                    src="/public/images/20210221-KXCoffeeGym29.jpg"
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="px-6 py-4 space-y-2">
+                  <div className="flex items-center text-sm">
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">
+                      star_half
+                    </span>
+                    <span className="ml-2 text-xs text-gray-600">
+                      294 Reviews
+                    </span>
+                  </div>
+                  <p className="text-lg font-bold text-gray-600">Grade Gym</p>
+                  <p className="text-sm text-gray-600">
+                    <span className="material-icons align-middle text-gray-500 -ml-1">
+                      place
+                    </span>
+                    {""} Somewhere, Uganda
+                  </p>
+                  <div className="flex gap-3">
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      clinic
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      hospital
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      $$
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-white rounded-md shadow mx-w-xl overflow-hidden">
+                <div className="h-48 overflow-hidden">
+                  <img
+                    src="/public/images/download.jpg"
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="px-6 py-4 space-y-2 relative">
+                  <div className="flex items-center text-sm">
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">
+                      star_half
+                    </span>
+                    <span className="ml-2 text-xs text-gray-600">
+                      294 Reviews
+                    </span>
+                  </div>
+                  <p className="text-lg font-bold text-gray-600">
+                    Insane Burgers
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    <span className="material-icons align-middle text-gray-500 -ml-1">
+                      place
+                    </span>
+                    {""} Somewhere, Uganda
+                  </p>
+                  <div className="flex gap-3">
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      clinic
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      hospital
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      $$
+                    </span>
+                  </div>
+                  <div className="absolute right-3 bottom-3">
+                    <span className="material-icons text-red-500">
+                      local_fire_department
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-white rounded-md shadow mx-w-xl overflow-hidden">
+                <div className="h-48 overflow-hidden">
+                  <img
+                    src="/public/images/images (1).jpg"
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="px-6 py-4 space-y-2">
+                  <div className="flex items-center text-sm">
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">
+                      star_half
+                    </span>
+                    <span className="ml-2 text-xs text-gray-600">
+                      294 Reviews
+                    </span>
+                  </div>
+                  <p className="text-lg font-bold text-gray-600">
+                    Dr. Someone Clinic
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    <span className="material-icons align-middle text-gray-500 -ml-1">
+                      place
+                    </span>
+                    {""} Somewhere, Uganda
+                  </p>
+                  <div className="flex gap-3">
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      clinic
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      hospital
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      $$
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-white rounded-md shadow mx-w-xl overflow-hidden">
+                <div className="h-48 overflow-hidden">
+                  <img
+                    src="/public/images/res-pic (7).jpg"
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="px-6 py-4 space-y-2">
+                  <div className="flex items-center text-sm">
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">
+                      star_half
+                    </span>
+                    <span className="ml-2 text-xs text-gray-600">
+                      294 Reviews
+                    </span>
+                  </div>
+                  <p className="text-lg font-bold text-gray-600">
+                    Fancy Restaurant
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    <span className="material-icons align-middle text-gray-500 -ml-1">
+                      place
+                    </span>
+                    {""} Somewhere, Uganda
+                  </p>
+                  <div className="flex gap-3">
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      clinic
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      hospital
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      $$
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-white rounded-md shadow mx-w-xl overflow-hidden">
+                <div className="h-48 overflow-hidden">
+                  <img
+                    src="/public/images/hero/hero_1.jpg"
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="px-6 py-4 space-y-2">
+                  <div className="flex items-center text-sm">
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">grade</span>
+                    <span className="material-icons text-amber-400">
+                      star_half
+                    </span>
+                    <span className="ml-2 text-xs text-gray-600">
+                      294 Reviews
+                    </span>
+                  </div>
+                  <p className="text-lg font-bold text-gray-600">
+                    Shopping Stall
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    <span className="material-icons align-middle text-gray-500 -ml-1">
+                      place
+                    </span>
+                    {""} Somewhere, Uganda
+                  </p>
+                  <div className="flex gap-3">
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      clinic
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      hospital
+                    </span>
+                    <span className="text-xs px-2 py-0.5 bg-amber-100 border border-amber-200 text-amber-700 rounded-full">
+                      $$
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 text-center">
+              <button className="rounded-md py-2.5 px-6 bg-white border border-gray-100 text-lg shadow hover:shadow-lg text-gray-600">
+                🚀Discover More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
