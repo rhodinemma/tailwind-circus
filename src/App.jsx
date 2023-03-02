9366ac881016806c988433ae93ada874529b30eb
rhodinemma <rhodinemma10@gmail.com>
2023-03-02 14:59:24 +0300

    feat: add discover by categories section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import PopularPlaces from "./components/PopularPlacesSection";
 import Hero from "./components/HeroSection";
 import NavBar from "./components/HeaderSection";
+import DiscoverByCategories from "./components/DiscoverByCategories";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Hero />
 
       <PopularPlaces />
+
+      <DiscoverByCategories />
     </>
   );
 }
